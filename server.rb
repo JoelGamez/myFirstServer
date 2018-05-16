c4d2cb1404c4ccf1f75780472b4abda9e89c2bc6
joel gamez <joelgamez14six@yahoo.com>
2018-05-16 12:07:00 -0400

    Initital commit

---
+++ server.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+
+
+get '/' do
+  '<h1>Hello World!</h1>'
+end
+
+get '/sinatra' do
+  '<h1>Come fly with me!</h1>'
+end
+
+#
+# put '/' do
+#   "Hello World"
+# end
+#
+#
+# post '/' do
+#   "Hello World"
+# end
+#
+#
+# delete '/' do
+#   "Hello World"
+# end
+#
